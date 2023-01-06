45c63d234425838adcb870d55cc50683e1b7b8a8
suitupMT <suitup.mt@gmail.com>
2023-01-06 05:30:09 -0600

    added folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './app.css'
+import Component from './components/Component'
 export default function App() {
   return (
     <div>
       <h1>This is a starting template</h1>
+      <Component/>
     </div>
   )
 }
---
+++ src/components/Component.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Component() {
+  return (
+    <div>
+      Modular Programming Here
+    </div>
+  )
+}
